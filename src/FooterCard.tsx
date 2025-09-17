d7fcb0405a985cf5b39e7dfbfea0eeff87d833f2
Josh <josh@joshlawrence.dev>
2025-09-17 15:05:38 -0400

    Update resume link in FooterCard component

--- src/FooterCard.tsx
+++ src/FooterCard.tsx
@@ -9,7 +9,7 @@ const FooterCard = () => {
       </a>
       <p className="text-neutral-600">|</p>
       <a
-        href="https://docs.google.com/document/d/1aiMQC6s7wtWikPZxbKy02qgMqEe05R4rRMk2g9OvRg0/edit?usp=sharing"
+        href="https://docs.google.com/document/d/1MdDLoQdBwiiFj7b7xI1iSWhh-75gnUxy0pwWHqVYOeE/edit?usp=sharing"
         className="text-neutral-600 hover:text-neutral-300"
       >
         resume
