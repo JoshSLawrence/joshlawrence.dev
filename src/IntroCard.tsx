9991126814150d199650c54e5328ae4f94708f77
Josh <josh@joshlawrence.dev>
2025-10-30 13:33:58 -0700

    Update job title in IntroCard component

--- src/IntroCard.tsx
+++ src/IntroCard.tsx
@@ -5,7 +5,7 @@ const IntroCard = () => {
         Josh Lawrence
       </h2>
       <h3 className="pb-2 text-left text-lg text-neutral-400">
-        Cloud Architect, Quadax Inc.
+        Senior Cloud Architect, DevOps @ Quadax Inc.
       </h3>
       <div className="flex flex-col gap-2">
         <p>Hey, thanks for stopping by!</p>
